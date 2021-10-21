02753db063696116b77ee2fce062d931edd15634
zsolt2 <79489624+zsolt2@users.noreply.github.com>
2021-10-21 19:41:33 +0200

    Task 3 part 2 solved

--- HelloWorld/Task3.cs
+++ HelloWorld/Task3.cs
@@ -7,25 +7,35 @@ namespace HelloWorld
 {
     class Task3 : Task
     {
-        public Task3(): base("../Inputs/3/input") { }
+        public Task3() : base("../Inputs/3/input") { }
         public override void Run()
         {
             this.readLines();
             int possibleTriangles = 0;
-            foreach ( string s in this.lines)
+            string[][] buffer = new string[3][];
+            int i = 0;
+            foreach (string s in this.lines)
             {
                 string trimmed = s.Trim();
                 Regex regex = new Regex(@"[ ]{2,}");    // Regex to match more than two occurences of space 
                 var sides = regex.Replace(trimmed, @" ").Split(' ');
-                
-                
-                int a = int.Parse(sides[0]);
-                int b = int.Parse(sides[1]);
-                int c = int.Parse(sides[2]);
-                if (isTriangle(a, b, c))
-                    possibleTriangles++;
+
+                buffer[i++] = sides;
+
+                if (i == 3)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        int a = int.Parse(buffer[0][k]);
+                        int b = int.Parse(buffer[1][k]);
+                        int c = int.Parse(buffer[2][k]);
+                        if (isTriangle(a, b, c))
+                            possibleTriangles++;
+                    }
+                    i = 0;
+                }
             }
-            Console.WriteLine("Task 3 part 1: " + possibleTriangles);
+            Console.WriteLine("Task 3 part 2: " + possibleTriangles);
         }
 
         private bool isTriangle(int a, int b, int c)
