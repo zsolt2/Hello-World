25b2275afdfda2a98372ea8388fbaf87dd9dc779
zsolt2 <79489624+zsolt2@users.noreply.github.com>
2021-10-21 16:04:01 +0200

    Create Framework for Tasks

---
+++ HelloWorld/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HelloWorld
+{
+    
+    abstract class Task : TaskInterface
+    {
+
+        protected string text;
+        protected string[] lines;
+        protected string filePath;
+        public abstract void Run();
+        public Task(string filePath)
+        {
+            this.filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName+ "\\" + filePath;
+        }
+        protected void readAllText() 
+        {
+            this.text =  System.IO.File.ReadAllText(this.filePath);
+        }
+
+        protected void readLines()
+        {
+            this.lines =  System.IO.File.ReadAllLines(this.filePath);
+        }
+        
+    }
+}
